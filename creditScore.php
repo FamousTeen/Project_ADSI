732333d890723e582ad5d08c07594ba7738711e4
SicBell <131933766+SicBell@users.noreply.github.com>
2024-06-04 20:46:36 +0700

    d

--- creditScore.php
+++ creditScore.php
@@ -209,7 +209,7 @@
     <div class="container">
         <aside class="sidebar">
             <ul>
-                <a style="text-decoration: none; color: inherit;" href="createProject_page"><li id="permitSide" >My Projects</li></a>
+                <a style="text-decoration: none; color: inherit;" href="createProject_page.php"><li id="permitSide" >My Projects</li></a>
                 <a style="text-decoration: none; color: inherit;" href="createPermitReq.php"><li id="permitSide" >Permit Request</li></a>
                 <a style="text-decoration: none; color: inherit;" href="progressBar.php"><li id="customSide" >Custom Project Progress</li></a>
                 <a style="text-decoration: none; color: inherit;" href="#"><li id="creditSide" class="active">Credit score & awards</li></a>
